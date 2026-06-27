d90846f872a74adeb8438b7a7c2101a7e883c289
frostehhh <frostehhh967@gmail.com>
2026-06-27 17:52:58 +0800

    Hide font errors

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.warn_about_missing_glyphs = false
+
 require("modules.appearance").apply(config)
 require("modules.keybindings").apply(config)
 require("modules.favorites").apply()
